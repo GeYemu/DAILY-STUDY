a471d5b66d8803b3c4bdb4fce9447a92f8effacd
ＧＥＹＥＭＵ <>
2024-04-15 12:39:06 +0800

    jrji

--- static示例.java
+++ static示例.java
@@ -7,7 +7,7 @@ public class static示例 {
     }
     
     public static void main(String[] args){
-    statictest st1 =new statictest();
+    //statictest st1 =new statictest();
     statictest st2 =new statictest();
     System.out.println(statictest.i);
     }
