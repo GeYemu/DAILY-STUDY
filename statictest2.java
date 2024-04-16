388d476426789ca816dfe396b0f4d1c229881c52
GeYemu <891196720@qq.com>
2024-04-16 09:32:09 +0800

    super

--- statictest2.java
+++ statictest2.java
@@ -7,9 +7,9 @@ class statictest2{
     public static void main(String[] args)
     {
         //A aa1=new A();
-        A aa2 = new A();
+        //A aa2 = new A();
         //aa1.j=40;
-        System.out.printf("%d\n",aa2.j);
+        //System.out.printf("%d\n",aa2.j);
         System.out.printf("%d\n",A.j);
     }
 }
