52da6f8580d09b1b25691bc0a0c69ce6ac2c577c
ＧＥＹＥＭＵ <>
2024-04-22 13:02:33 +0800

    jar

---
+++ Mathlib/src/com/example/math/MathTools.java
@@ -0,0 +1,12 @@
+// 文件路径: MathLib/src/com/example/math/MathTools.java
+package com.example.math;
+
+public class MathTools {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
